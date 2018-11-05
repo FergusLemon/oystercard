f3d89b152747204f49f7c32810d24c3b97e2fd41
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-05 15:24:31 +0000

    Test and implement #balance on initialization of an oystercard.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,16 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:oystercard) { described_class.new }
 
+  context "on initialization" do
+    context "#balance" do
+      it 'has a balance' do
+        expect(oystercard).to respond_to(:balance)
+      end
+      it 'has a default balance of zero' do
+        expect(oystercard.balance).to equal(0)
+      end
+    end
+  end
 end
