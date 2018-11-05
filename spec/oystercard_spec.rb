0a8da1b8961ad14234c22c29e87f275bd9fe6903
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-05 15:43:25 +0000

    Minor update - change to single quotes.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,16 +4,16 @@ describe Oystercard do
   let(:oystercard) { described_class.new }
   let(:oystercard_10) { described_class.new(10) }
 
-  context "on initialization" do
-    context "#balance" do
+  context 'on initialization' do
+    context '#balance' do
       it 'has a balance' do
         expect(oystercard).to respond_to(:balance)
       end
       it 'has a default balance of zero' do
-        expect(oystercard.balance).to equal(0)
+        expect(oystercard.balance).to eq(0)
       end
       it 'can be chosen by the user when the card is purchased' do
-        expect(oystercard_10.balance).to equal(10)
+        expect(oystercard_10.balance).to eq(10)
       end
     end
   end
