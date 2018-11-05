caa5c1e4793cc1efeb097a97a0aa6f9c1b87e429
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-05 15:58:19 +0000

    Test and implement #top_up.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,7 @@ describe Oystercard do
   let(:oystercard_10) { described_class.new(10) }
 
   context 'on initialization' do
-    context '#balance' do
+    describe '#balance' do
       it 'has a balance' do
         expect(oystercard).to respond_to(:balance)
       end
@@ -14,6 +14,18 @@ describe Oystercard do
       end
       it 'can be chosen by the user when the card is purchased' do
         expect(oystercard_10.balance).to eq(10)
+      end
+    end
+  end
+
+  context 'when topping up' do
+    describe '#top_up' do
+      it 'can be topped up by a user' do
+        expect(oystercard).to respond_to(:top_up).with(1).argument
+      end
+      it 'increases the balance on the card by the amount topped up' do
+        oystercard.top_up(20)
+        expect(oystercard.balance).to eq(20)
       end
     end
   end
