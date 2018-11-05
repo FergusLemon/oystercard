78bf7e258ccc706e1ab68f57e016cff5126c555d
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-05 15:29:49 +0000

    Test and implement usee defined #balance on initialization.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   let(:oystercard) { described_class.new }
+  let(:oystercard_10) { described_class.new(10) }
 
   context "on initialization" do
     context "#balance" do
@@ -10,6 +11,9 @@ describe Oystercard do
       end
       it 'has a default balance of zero' do
         expect(oystercard.balance).to equal(0)
+      end
+      it 'can be chosen by the user when the card is purchased' do
+        expect(oystercard_10.balance).to equal(10)
       end
     end
   end
