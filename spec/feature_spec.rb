3c2171bf19391bf146518520536fee092394fdd6
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-06 17:17:23 +0000

    Pass user story #6.

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,48 +1,56 @@
-require 'oystercard'
+require './lib/oystercard'
 # In order to use public transport
 # As a customer
 # I want money on my card
-puts 'oystercard = Oystercard.new'
+puts '>> oystercard = Oystercard.new'
 puts oystercard = Oystercard.new
-puts 'oystercard10 = Oystercard.new(10)'
+puts '>> oystercard10 = Oystercard.new(10)'
 puts oystercard10 = Oystercard.new(10)
 #
 # In order to keep using public transport
 # As a customer
 # I want to add money to my card
-puts 'oystercard.top_up(20)'
+puts '>> oystercard.top_up(20)'
 puts oystercard.top_up(20)
-puts 'oystercard.balance'
+puts '>> oystercard.balance'
 puts oystercard.balance
 #
 # In order to protect my money
 # As a customer
 # I don't want to put too much money on my card
-puts 'oystercard.top_up(100)'
+puts '>> oystercard.top_up(100)'
 # puts oystercard.top_up(100)
+puts 'RuntimeError: Sorry the maximum balance is 90, please try topping up a lower amount.'
 #
 # In order to pay for my journey
 # As a customer
 # I need my fare deducted from my card
-puts 'oystercard.deduct(10)'
+puts '>> oystercard.deduct(10)'
 puts oystercard.deduct(10)
-puts 'oystercard.balance'
+puts '>> oystercard.balance'
 puts oystercard.balance
-puts
-puts 'oystercard.deduct(50)'
-puts oystercard.deduct(50)
+puts '>> oystercard.deduct(50)'
+# puts oystercard.deduct(50)
+puts 'RuntimeError: Your balance is 10, you do not have enough for this transaction.'
 #
 # In order to get through the barriers
 # As a customer
 # I need to touch in and out
-puts 'oystercard.touch_in'
+puts '>> oystercard.touch_in'
 puts oystercard.touch_in
-puts 'oystercard.touch_out'
+puts '>> oystercard.touch_out'
 puts oystercard.touch_out
 #
 # In order to pay for my journey
 # As a customer
 # I need to have the minimum amount for a single journey
+puts '>> another_oystercard = Oystercard.new'
+puts another_oystercard = Oystercard.new
+puts '>> another_oystercard.balance'
+puts another_oystercard.balance
+puts '>> another_oystercard.touch_in'
+# puts another_oystercard.touch_in
+puts 'RuntimeError: Your balance (£0) is insufficient, you need a balance of £1 to travel.'
 #
 # In order to pay for my journey
 # As a customer
