57de1b296ecde67d564a6e46e17380b6cf9b912f
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-07 13:30:32 +0000

    Refactor - remove superfluous tests.

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,18 +4,12 @@ describe Station do
   let(:station) { described_class.new(:clapham_north, 2) }
 
   describe '#name' do
-    it 'responds to name' do
-      expect(station).to respond_to(:name)
-    end
     it 'knows its own name' do
       expect(station.name).to eq(:clapham_north)
     end
   end
 
   describe '#zone' do
-    it 'responds to zone' do
-      expect(station).to respond_to(:zone)
-    end
     it 'knows its own zone' do
       expect(station.zone).to eq(2)
     end
