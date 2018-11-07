9ba9b01a8d730513a7c23ffdcbecf16e78397ca0
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-07 13:29:02 +0000

    Test and implement #zone.

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -11,4 +11,13 @@ describe Station do
       expect(station.name).to eq(:clapham_north)
     end
   end
+
+  describe '#zone' do
+    it 'responds to zone' do
+      expect(station).to respond_to(:zone)
+    end
+    it 'knows its own zone' do
+      expect(station.zone).to eq(2)
+    end
+  end
 end
