86ee0397b75c91c387595558f116d42fa590b4b5
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-14 11:03:37 +0000

    Remove superflous methods now implemented in other classes and delete related specs.

--- lib/journey.rb
+++ lib/journey.rb
@@ -18,20 +18,8 @@ class Journey
     penalty? ? PENALTY_FARE : MIN_FARE
   end
 
-  def calculate_penalty
-    PENALTY_FARE
-  end
-
   def complete?
     @complete
-  end
-
-  def was_expecting_touch_in
-    self.complete? || (self.entry_station == nil && self.exit_station != nil)
-  end
-
-  def was_expecting_touch_out
-    self.entry_station != nil && self.exit_station == nil
   end
 
   private
