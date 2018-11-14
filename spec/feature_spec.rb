5fd39c67e7fc8762f93d49bdd0b57b4333e90c68
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-14 13:52:04 +0000

    Pass user story #12.

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -25,23 +25,24 @@ puts 'RuntimeError: Sorry the maximum balance is 90, please try topping up a low
 # In order to pay for my journey
 # As a customer
 # I need my fare deducted from my card
+puts '>> station = Station.new(:clapham_south, 2)'
+puts station = Station.new(:clapham_south, 2)
+puts '>> another_station = Station.new(:holburn, 2)'
+puts another_station = Station.new(:holburn, 2)
 puts '>> oystercard.touch_in(:london_bridge)'
-puts oystercard.touch_in(:london_bridge)
+puts oystercard.touch_in(station)
 puts '>> oystercard.touch_out(:southwark)'
-puts oystercard.touch_out(:southwark)
+puts oystercard.touch_out(another_station)
 puts '>> oystercard.balance'
 puts oystercard.balance
-# puts '>> oystercard.deduct(50)'
-# puts oystercard.deduct(50)
-# puts 'RuntimeError: Your balance is 10, you do not have enough for this transaction.'
 #
 # In order to get through the barriers
 # As a customer
 # I need to touch in and out
-puts '>> oystercard.touch_in(:london_bridge)'
-puts oystercard.touch_in(:london_bridge)
-puts '>> oystercard.touch_out(:southwark)'
-puts oystercard.touch_out(:southwark)
+puts '>> oystercard.touch_in(staion)'
+puts oystercard.touch_in(station)
+puts '>> oystercard.touch_out(another_station)'
+puts oystercard.touch_out(another_station)
 #
 # In order to pay for my journey
 # As a customer
@@ -50,25 +51,25 @@ puts '>> another_oystercard = Oystercard.new'
 puts another_oystercard = Oystercard.new
 puts '>> another_oystercard.balance'
 puts another_oystercard.balance
-puts '>> another_oystercard.touch_in(:london_bridge)'
+puts '>> another_oystercard.touch_in(station)'
 # puts another_oystercard.touch_in
 puts 'RuntimeError: Your balance (£0) is insufficient, you need a balance of £1 to travel.'
 #
 # In order to pay for my journey
 # As a customer
 # I need to pay for my journey when it's complete
-puts '>> oystercard.touch_in(:london_bridge)'
-puts oystercard.touch_in(:london_bridge)
-puts '>> oystercard.touch_out(:southwark)'
-puts oystercard.touch_out(:southwark)
+puts '>> oystercard.touch_in(station)'
+puts oystercard.touch_in(station)
+puts '>> oystercard.touch_out(another_station)'
+puts oystercard.touch_out(another_station)
 puts '>> oystercard.balance'
 puts oystercard.balance
 #
 # In order to pay for my journey
 # As a customer
 # I need to know where I've travelled from
-puts '>> oystercard.touch_in(:euston)'
-puts oystercard.touch_in(:euston)
+puts '>> oystercard.touch_in(station)'
+puts oystercard.touch_in(station)
 puts '>> oystercard'
 p oystercard
 #
@@ -87,17 +88,28 @@ puts station = Station.new(:clapham_south, 2)
 # In order to be charged correctly
 # As a customer
 # I need a penalty charge deducted if I fail to touch in or out
-puts '>> oystercard.touch_out(:southwark)'
-puts oystercard.touch_out(:southwark)
+puts '>> oystercard.touch_out(another_station)'
+puts oystercard.touch_out(another_station)
 puts '>> oystercard.balance'
 puts oystercard.balance
-puts '>> oystercard.touch_in(:euston)'
-puts oystercard.touch_in(:euston)
-puts '>> oystercard.touch_in(:euston)'
-puts oystercard.touch_in(:euston)
+puts '>> oystercard.touch_in(station)'
+puts oystercard.touch_in(station)
+puts '>> oystercard.touch_in(station)'
+puts oystercard.touch_in(station)
 puts '>> oystercard.balance'
 puts oystercard.balance
+puts '>> oystercard.touch_out(another_station)'
+puts oystercard.touch_out(another_station)
+#
 #
 # In order to be charged the correct amount
 # As a customer
 # I need to have the correct fare calculated
+puts '>> other_station = Station.new(:tooting, 6)'
+puts other_station = Station.new(:tooting, 6)
+puts '>> oystercard.touch_in(station)'
+puts oystercard.touch_in(station)
+puts '>> oystercard.touch_out(other_station)'
+puts oystercard.touch_out(other_station)
+puts '>> oystercard.balance'
+puts oystercard.balance
